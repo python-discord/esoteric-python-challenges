699e7f7e882dbad38b9f8e760113c61d568ab7a2
aaay-aaay <32845331+aaay-aaay@users.noreply.github.com>
2019-03-30 15:19:02 +0000

    Exit instead of SyntaxError
    Instead of raising a SyntaxError, just exit. Not the best, but it means it won't break Maybe.

--- challenges/06-maybe-keyword/solutions/pastebin.py
+++ challenges/06-maybe-keyword/solutions/pastebin.py
@@ -4,8 +4,8 @@ def t(f,e,a):
  if e=='call':
   c=f.f_code
   for i,o in enumerate(c.co_code[::2]):
-   if o in b'Z[ab'and c.co_names[c.co_code[i*2+1]]=='Maybe':raise SyntaxError
-   elif o in b'}~'and c.co_varnames[c.co_code[i*2+1]]=='Maybe':raise SyntaxError
+   if o in b'Z[ab'and c.co_names[c.co_code[i*2+1]]=='Maybe':exit()
+   elif o in b'}~'and c.co_varnames[c.co_code[i*2+1]]=='Maybe':exit()
  f.f_locals['Maybe']=bool(random.choice([1,0]))
  return t
 sys.settrace(t)
