e58507872426c76dbb23ab59ea66e8d64b177e54
aaay-aaay <32845331+aaay-aaay@users.noreply.github.com>
2019-03-30 07:12:36 +0000

    Add our solution to challenge 6

---
+++ challenges/06-maybe-keyword/solutions/pastebin.py
@@ -0,0 +1,11 @@
+import sys,random
+def t(f,e,a):
+ # 3.7 ONLY: f.f_trace_opcodes=True
+ if e=='call':
+  c=f.f_code
+  for i,o in enumerate(c.co_code[::2]):
+   if o in b'Z[ab'and c.co_names[c.co_code[i*2+1]]=='Maybe':raise SyntaxError
+   elif o in b'}~'and c.co_varnames[c.co_code[i*2+1]]=='Maybe':raise SyntaxError
+ f.f_locals['Maybe']=bool(random.choice([1,0]))
+ return t
+sys.settrace(t)
