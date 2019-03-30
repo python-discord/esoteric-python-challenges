54b28671d704710df5fb1f676edcc7a98a0c3e4a
aaay-aaay <32845331+aaay-aaay@users.noreply.github.com>
2019-03-30 20:04:19 +0000

    Golf it a bit more

--- challenges/06-maybe-keyword/solutions/pastebin.py
+++ challenges/06-maybe-keyword/solutions/pastebin.py
@@ -1,12 +1,13 @@
 import sys,random
+m='Maybe'
 def t(f,e,a):
  # 3.7 ONLY: f.f_trace_opcodes=True
  if e=='call':
   c=f.f_code
   for i,o in enumerate(c.co_code[::2]):
-   if o in b'Z[ab'and c.co_names[c.co_code[i*2+1]]=='Maybe':exit()
-   elif o in b'}~'and c.co_varnames[c.co_code[i*2+1]]=='Maybe':exit()
- f.f_locals['Maybe']=bool(random.choice([1,0]))
+   x=c.co_code[i*2+1]
+   if o in b'Z[ab'and c.co_names[x]==m or o in b'}~'and c.co_varnames[x]==m:exit()
+ f.f_locals[m]=bool(random.choice([1,0]))
  return t
 sys.settrace(t)
 
