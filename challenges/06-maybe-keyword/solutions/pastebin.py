a6698df570cab52a186d9cfd6de6ab0f51088781
aaay-aaay <32845331+aaay-aaay@users.noreply.github.com>
2019-03-30 19:54:53 +0000

    Add ungolfed version

--- challenges/06-maybe-keyword/solutions/pastebin.py
+++ challenges/06-maybe-keyword/solutions/pastebin.py
@@ -9,3 +9,23 @@ def t(f,e,a):
  f.f_locals['Maybe']=bool(random.choice([1,0]))
  return t
 sys.settrace(t)
+
+'''
+Ungolfed version
+import sys, random
+def trace(frame, event, arg): # settrace stuff
+    # 3.7 ONLY: f.f_trace_opcodes=True
+    if event == 'call': # function just got called
+        code = frame.f_code
+        for index, opcode in enumerate(code.co_code[::2]): # only the opcodes, not the args
+            # Opcodes in b'Z[ab' perform assignment/deletion on a thing from co_names.
+            # If it's messing with Maybe, we don't want that.
+            if opcode in b'Z[ab' and code.co_names[code.co_code[index * 2 + 1]] == 'Maybe':
+                exit()
+            # Same for co_varnames
+            elif opcode in b'}~' and code.co_varnames[code.co_code[index * 2 + 1]] == 'Maybe':
+                exit()
+    frame.f_locals['Maybe'] = bool(random.choice([1, 0])) # Set Maybe in the frame's locals to a random bool
+    return trace # settrace stuff
+sys.settrace(trace)
+'''
