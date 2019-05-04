c75ff702a76d92ec6a38475578d27e599ca01f12
BuildTools <unconfigured@null.spigotmc.org>
2019-05-04 08:27:50 +0100

    add EdgyNerd's solution

---
+++ challenges/07-code-art/solutions/EdgyNerd.py
@@ -0,0 +1,20 @@
+# EdgyNerd's Solution: Mario Question Mark Block
+
+print(__import__('random').choice(
+[(                             '1'
+)+      chr(int(ord('ǂ')/       10
+))    +chr(            5*17      )
++     chr(2            *56)      ,
+''    +chr(            7*11      )
++     chr(             5*23     +2
+)+                 chr(5*       23
+)+            chr(10*           10
++4            )+chr(            5*
+22            +2*2)+            ''
++             chr(2*            56
+-1            )+chr(            37
+*                                3
+)+            chr(99             +                         
+10            ),chr(            83
+)+                              ''
++chr(4*29)+chr(4*24+1)+chr(114)]))
